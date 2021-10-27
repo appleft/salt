2d5876274af5b07130dce30bac568ccd40e7319a
serveto <serveto@cc.appleft.xyz>
2021-10-28 02:25:09 +0800

    UI

--- init.el
+++ init.el
@@ -75,3 +75,60 @@
 ;;; provides `straight-x-clean-unused-repos' (part of `straight.el')
 (use-package straight-x
   :defer t)
+
+;;; modus themes
+(use-package modus-themes
+  :straight t
+  :init
+  (setq modus-themes-no-mixed-fonts t
+        modus-themes-slanted-constructs t
+        modus-themes-bold-constructs t
+        modus-themes-fringes 'subtle    ; {nil,'subtle,'intense}
+        modus-themes-mode-line '3d      ; {nil,'3d,'moody}
+        modus-themes-syntax 'yellow-comments-green-strings ; Lots of options---continue reading the manual
+        modus-themes-intense-hl-line nil
+        modus-themes-paren-match 'subtle-bold ; {nil,'subtle-bold,'intense,'intense-bold}
+        modus-themes-links 'neutral-underline ; Lots of options---continue reading the manual
+        modus-themes-prompts 'intense         ; {nil,'subtle,'intense}
+        modus-themes-completions 'moderate      ; {nil,'moderate,'opinionated}
+        modus-themes-region nil ; {nil,'no-extend,'bg-only,'bg-only-no-extend}
+        modus-themes-diffs nil  ; {nil,'desaturated,'fg-only,'bg-only}
+        modus-themes-org-blocks 'grayscale      ; {nil,'grayscale,'rainbow}
+        modus-themes-headings ; Lots of options---continue reading the manual
+        '((1 . section)
+          (2 . section-no-bold)
+          (3 . rainbow-line)
+          (t . rainbow-line-no-bold))
+        modus-themes-variable-pitch-headings nil
+        modus-themes-scale-headings nil
+        modus-themes-scale-1 1.1
+        modus-themes-scale-2 1.15
+        modus-themes-scale-3 1.21
+        modus-themes-scale-4 1.27
+        modus-themes-scale-5 1.33)
+  (set-face-attribute 'default nil :family "M+ 1mn Light" :height 200)
+  (set-face-attribute 'variable-pitch nil :family "M+ 1mn Light" :height 200)
+  (set-face-attribute 'variable-pitch nil :family "M+ 1mn Light" :height 200)
+  :config
+  (load-theme 'modus-vivendi t))
+
+;;; display line number
+(use-package display-line-numbers
+  :hook (prog-mode . display-line-numbers-mode)
+  :hook (text-mode . display-line-numbers-mode))
+
+;;; TODO: deal with word-wrap issue of language-mixed style.
+;;; visual-fill-column
+(use-package visual-fill-column
+  :straight t
+  :init
+  (setq visual-fill-column-inhibit-sensible-window-split t
+        visual-fill-column-width 120)
+  :hook
+  (text-mode . visual-line-mode)
+  (prog-mode . visual-line-mode)
+  (visual-line-mode . visual-fill-column-mode)
+  :config
+  (add-hook 'visual-line-mode-hook #'(lambda ()
+                                       (setq-local word-wrap nil)))
+  (advice-add 'text-scale-adjust :after #'visual-fill-column-adjust))
